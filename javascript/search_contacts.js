5710f58cdaa5456950fe1b8e3923fa276a4f6efd
Johnson L. <johnsonlaguerre@outlook.com>
2025-01-20 22:14:09 -0500

    Outline for Search_contacts JS file.

---
+++ javascript/search_contacts.js
@@ -0,0 +1,55 @@
+// // JavaScript - Search_contacts-page
+
+// // Preventing form refresh
+// const form = document.querySelector('.form');
+// form.addEventListener('submit', (e) => {
+    // e.preventDefault(); // Prevent the form from refreshing the page
+// });
+
+// // References to Buttons
+// const createButton = document.getElementById('create-btn');
+// const updateButton = document.getElementById('update-btn');
+// const deleteButton = document.getElementById('delete-btn');
+
+// // Create Button Listener
+// createButton.addEventListener('click', () => {
+	
+	// // Collect field information
+    // const firstName = document.getElementById('first-name').value;
+    // const lastName = document.getElementById('last-name').value;
+    // const email = document.getElementById('email').value;
+    // const phoneNumber = document.getElementById('phone-number').value;
+	
+    // // All fields blank
+    // if (firstName === '' && lastName === '' && email === '' && phoneNumber === '') {
+        // const errorMessage = document.getElementById('error-message');
+		// errorMessage.textContent = 'Please enter information in at least one field.';
+        // errorMessage.style.display = 'block'; //Show error
+        
+    // }
+	
+	// // Field Given - Validate Record
+    	// // Waiting on API
+	// // Search for record
+		// // Record found
+		// // if (record_found) {
+			// // Display in contact box
+		// // }
+		// // Record not found
+		// // if (!record_found) {
+			// // const errorMessage = document.getElementById('error-message');
+		 	// // errorMessage.textContent = 'Save record when complete.';
+	     // // errorMessage.style.display = 'block'; //Show error
+		// // }
+	
+// });
+
+// // Update Button Listener
+// updateButton.addEventListener('click', () => {
+	
+// });
+
+// // Delete Button Listener
+// deleteButton.addEventListener('click', () => {
+	
+// });
