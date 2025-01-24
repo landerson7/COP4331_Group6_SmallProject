85556c5451a71ca135d5c00ee918cb41a4f3e0e6
prestons21 <pstrow03@gmail.com>
2025-01-23 19:21:34 -0500

    Create deleteContact.php
    Calls for contact deletion using current userID, and first + last name of contact to be deleted

---
+++ deleteContact.php
@@ -0,0 +1,57 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError($conn->connect_error);
+	} 
+	else 
+	{
+		//delete calls from just the first/last names
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName=? AND LastName=? AND UserID=?");
+		$stmt->bind_param("ssi", $firstName, $lastName, $userId);
+		$stmt->execute();
+
+		if ($stmt->affected_rows > 0) 
+		{
+			returnWithSuccess("Contact deleted successfully");
+		} 
+		else 
+		{
+			returnWithError("No matching contact found for this user");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithSuccess($msg)
+	{
+		$retValue = '{"message":"' . $msg . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+?>
